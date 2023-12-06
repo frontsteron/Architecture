a647caef87f5cce8c0b5b4edf8823547394b9524
Denis Yershov <frontsteron@gmail.com>
2023-12-06 14:29:07 +0500

    fix: remove model

--- TheEditor/src/main/java/ru/geekbrains/lesson5/Database.java
+++ TheEditor/src/main/java/ru/geekbrains/lesson5/Database.java
@@ -2,14 +2,10 @@ package ru.geekbrains.lesson5;
 
 import java.util.Collection;
 
-/**
- * Интерфейс БД
- */
 public interface Database {
-
     void load();
     void save();
-
     Collection<Entity> getAll();
-
+    void addModel(Model3D model);
+    void removeModel(Model3D model);
 }
