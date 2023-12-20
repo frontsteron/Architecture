985379b1ae5cf3db7d67935a397b4a715cde94ac
Denis Yershov <frontsteron@gmail.com>
2023-12-20 12:18:13 +0500

    fix: added task

---
+++ MyFirstWebApplication/MyFirstWebApplication/Models/WeatherForecast.cs
@@ -0,0 +1,34 @@
+﻿namespace MyFirstWebApplication.Models
+{
+    /// <summary>
+    /// Прогноз погоды Forecast
+    /// </summary>
+    public class WeatherForecast
+    {
+        /// <summary>
+        /// Температура в градус Цельсия
+        /// </summary>
+        public int TemperatureC { get; set; }
+
+        /// <summary>
+        /// Дата измерения
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        public WeatherForecast(DateTime date, int temperatureC)
+        {
+            Date = date;
+            TemperatureC = temperatureC;
+        }
+
+        /// <summary>
+        /// Температура в градус Форенгейта
+        /// </summary>
+        public int TemperatureF
+        {
+            get { return 32 + (int)(TemperatureC / 0.5556); }
+        }
+
+
+    }
+}
---
+++ MyFirstWebApplication/MyFirstWebApplication/Program.cs
@@ -0,0 +1,37 @@
+
+using MyFirstWebApplication.Models;
+
+namespace MyFirstWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<WeatherForecastHolder>();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
