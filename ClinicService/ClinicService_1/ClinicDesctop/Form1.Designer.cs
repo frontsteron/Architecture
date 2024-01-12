1abf75984536ce0524eb4c47d48005b9c4baaefd
Denis Yershov <frontsteron@gmail.com>
2024-01-12 18:06:25 +0500

    fix: added hw

---
+++ ClinicService/ClinicService_1/ClinicDesctop/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ClinicDesctop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listViewClients = new ListView();
+            buttonLoadClients = new Button();
+            columnHeaderId = new ColumnHeader();
+            columnHeaderSurName = new ColumnHeader();
+            columnHeaderFirstName = new ColumnHeader();
+            columnHeaderPatronymic = new ColumnHeader();
+            columnHeaderDocument = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // listViewClients
+            // 
+            listViewClients.Columns.AddRange(new ColumnHeader[] { columnHeaderId, columnHeaderSurName, columnHeaderFirstName, columnHeaderPatronymic, columnHeaderDocument });
+            listViewClients.FullRowSelect = true;
+            listViewClients.GridLines = true;
+            listViewClients.Location = new Point(12, 12);
+            listViewClients.MultiSelect = false;
+            listViewClients.Name = "listViewClients";
+            listViewClients.Size = new Size(776, 262);
+            listViewClients.TabIndex = 0;
+            listViewClients.UseCompatibleStateImageBehavior = false;
+            listViewClients.View = View.Details;
+            // 
+            // buttonLoadClients
+            // 
+            buttonLoadClients.Location = new Point(680, 280);
+            buttonLoadClients.Name = "buttonLoadClients";
+            buttonLoadClients.Size = new Size(108, 44);
+            buttonLoadClients.TabIndex = 1;
+            buttonLoadClients.Text = "Загрузить";
+            buttonLoadClients.UseVisualStyleBackColor = true;
+            buttonLoadClients.Click += buttonLoadClients_Click;
+            // 
+            // columnHeaderId
+            // 
+            columnHeaderId.Text = "#";
+            // 
+            // columnHeaderSurName
+            // 
+            columnHeaderSurName.Text = "Фамилия";
+            columnHeaderSurName.Width = 120;
+            // 
+            // columnHeaderFirstName
+            // 
+            columnHeaderFirstName.Text = "Имя";
+            columnHeaderFirstName.Width = 200;
+            // 
+            // columnHeaderPatronymic
+            // 
+            columnHeaderPatronymic.Text = "Отчество";
+            columnHeaderPatronymic.Width = 200;
+            // 
+            // columnHeaderDocument
+            // 
+            columnHeaderDocument.Text = "Документ";
+            columnHeaderDocument.Width = 200;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonLoadClients);
+            Controls.Add(listViewClients);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Моя клиника";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listViewClients;
+        private Button buttonLoadClients;
+        private ColumnHeader columnHeaderId;
+        private ColumnHeader columnHeaderSurName;
+        private ColumnHeader columnHeaderFirstName;
+        private ColumnHeader columnHeaderPatronymic;
+        private ColumnHeader columnHeaderDocument;
+    }
+}
