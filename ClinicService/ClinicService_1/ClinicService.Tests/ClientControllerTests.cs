6f27854f6689e46e25c7d3b4f01e01e0b8831ebf
Denis Yershov <frontsteron@gmail.com>
2024-01-14 14:04:51 +0500

    fix: added tests

--- ClinicService/ClinicService_1/ClinicService.Tests/ClientControllerTests.cs
+++ ClinicService/ClinicService_1/ClinicService.Tests/ClientControllerTests.cs
@@ -19,7 +19,7 @@ namespace ClinicService.Tests
 
         public ClientControllerTests()
         {
-           _mockClientRepository = new Mock<IClientRepository>();
+            _mockClientRepository = new Mock<IClientRepository>();
             _clientController = new ClientController(_mockClientRepository.Object);
         }
 
@@ -27,80 +27,102 @@ namespace ClinicService.Tests
         [Fact]
         public void GetAllClientsTest()
         {
-            // [1] Подготовка данных для тестирования
+            // [1.1] Подготовка данных для тестирования
+
+            //[1.2]
 
             List<Client> clientsList = new List<Client>();
             clientsList.Add(new Client());
             clientsList.Add(new Client());
             clientsList.Add(new Client());
 
-            _mockClientRepository
-                .Setup(repository => repository.GetAll())
-                .Returns(clientsList);
+            _mockClientRepository.Setup(repository =>
+                repository.GetAll()
+            ).Returns(clientsList);
 
-            // [2] Исполнение тестируемого метода
+            //[2] Исполнение тестируемого метода
 
 
             var operationResult = _clientController.GetAll();
 
-            // [3] Подготовка эталонного результата и проверка результата
+            //[3] Подготовка эталонного результата, проверка результата
             Assert.IsType<OkObjectResult>(operationResult.Result);
 
-            var result = (OkObjectResult)operationResult.Result;
-            Assert.IsAssignableFrom<List<Client>>(result.Value);
-
-            _mockClientRepository.Verify(repository =>
-            repository.GetAll(), Times.AtLeastOnce);
+            Assert.IsAssignableFrom<List<Client>>(((OkObjectResult)operationResult.Result).Value);
+            _mockClientRepository.Verify(repository => repository.GetAll(), Times.AtLeastOnce());
 
         }
 
-        public static object[][] CorrectCreateClientData =
+        // Подготовка для тестирования создания
+        public static readonly object[][] CorrectCreateClientData =
         {
-            new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов1", "Станислав1", "Андреевич1" },
-            new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов2", "Станислав2", "Андреевич2" },
-            //new object[] { new DateTime(2013, 1, 22), "AA1 B11222", "Иванов3", "Станислав3", "Андреевич3" },
-            new object[] { new DateTime(2018, 1, 22), "AA1 B11222", "Иванов4", "Станислав4", "Андреевич4" },
-            new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов5", "Станислав5", "Андреевич5" },
-            //new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов", "", "Андреевич" },
-            new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов6", "Станислав6", "Андреевич6" },
-            new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов7", "Станислав7", "Андреевич7" },
-            new object[] { new DateTime(1986, 1, 22), "AA1 B11222", "Иванов8", "Станислав8", "Андреевич8" },
-
+            new object []{ new DateTime(1985, 5, 20), "1234 123456", "Иванов", "Иван", "Иванович"},
+            new object []{ new DateTime(1987, 2, 18), "1234 321456", "Иванов", "Иван", "Иванович"},
+            new object []{ new DateTime(1975, 2, 22), "1234 123654", "Иванов", "Иван", "Иванович"},
         };
 
         [Theory]
         [MemberData(nameof(CorrectCreateClientData))]
-        public void CreateClientTest(
-            DateTime birthday, string document, string surName, string firstName, string patronymic)
+        public void CreateclientsListTest(DateTime birthday, string document, string surName, string firstName, string patronymic)
         {
-            // [1] Подготовка данных для тестирования
-
-            CreateClientRequest createClientRequest = new CreateClientRequest();
-            createClientRequest.Birthday = birthday;
-            createClientRequest.Document = document;
-            createClientRequest.SurName = surName;
-            createClientRequest.FirstName = firstName;
-            createClientRequest.Patronymic = patronymic;
-
-            int res = 1;
-
-            _mockClientRepository
-                .Setup(repository => repository.Create(It.IsNotNull<Client>()))
-                .Returns(res);
+            _mockClientRepository.Setup(repository => repository.Create(It.IsNotNull<Client>())).Returns(1).Verifiable();
 
 
-            // [2] Исполнение тестируемого метода
-            var operationResult = _clientController.Create(createClientRequest);
+            var operationResult = _clientController.Create(new CreateClientRequest
+            {
+                Birthday = birthday,
+                Document = document,
+                SurName = surName,
+                FirstName = firstName,
+                Patronymic = patronymic
+            });
 
-            // [3] Подготовка эталонного результата и проверка результата
             Assert.IsType<OkObjectResult>(operationResult.Result);
-
-            var result = (OkObjectResult)operationResult.Result;
-            Assert.IsAssignableFrom<int>(result.Value);
-
-            _mockClientRepository.Verify(repository =>
-            repository.Create(It.IsNotNull<Client>()), Times.AtLeastOnce());
+            Assert.IsAssignableFrom<int>(((OkObjectResult)operationResult.Result).Value);
+            _mockClientRepository.Verify(repository => repository.Create(It.IsNotNull<Client>()), Times.AtLeastOnce());
         }
 
+        // Подготовка для тестирования обновления
+        public static readonly object[][] CorrectUpdateClientData =
+       {
+            new object []{ new DateTime(1985, 5, 20), "1234 123456", "Иванов", "Иван", "Иванович", 1},
+            new object []{ new DateTime(1987, 2, 18), "1234 321456", "Иванов", "Иван", "Иванович", 2},
+            new object []{ new DateTime(1975, 2, 22), "1234 123654", "Иванов", "Иван", "Иванович", 3},
+        };
+
+        [Theory]
+        [MemberData(nameof(CorrectUpdateClientData))]
+        public void UpdateclientsListTest(DateTime birthday, string document, string surName, string firstName, string patronymic, int id)
+        {
+            _mockClientRepository.Setup(repository => repository.Update(It.IsNotNull<Client>())).Returns(1).Verifiable();
+
+
+            var operationResult = _clientController.Update(new UpdateClientRequest
+            {
+                ClientId = id,
+                Birthday = birthday,
+                Document = document,
+                SurName = surName,
+                FirstName = firstName,
+                Patronymic = patronymic
+            });
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.IsAssignableFrom<int>(((OkObjectResult)operationResult.Result).Value);
+            _mockClientRepository.Verify(repository => repository.Update(It.IsNotNull<Client>()), Times.AtLeastOnce());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void DeleteclientsListTest(int id)
+        {
+            _mockClientRepository.Setup(repository => repository.Delete(id)).Returns(id);
+
+            var operationResult = _clientController.Delete(id);
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.IsAssignableFrom<int>(((OkObjectResult)operationResult.Result).Value);
+            _mockClientRepository.Verify(repository => repository.Delete(id), Times.AtLeastOnce());
+        }
     }
 }
